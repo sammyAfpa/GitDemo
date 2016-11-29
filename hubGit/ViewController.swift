0b2096fdf442732230e64c50d04890c7ac58c45e
HILALA SAMI <hilalasami@mbp-de-hilala.afpa-paris-di>
2016-11-29 09:01:47 +0100

    Initial Commit

---
+++ hubGit/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  hubGit
+//
+//  Created by HILALA SAMI on 29/11/2016.
+//  Copyright © 2016 HILALA SAMI. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
